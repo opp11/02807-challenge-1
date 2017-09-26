b9250a1632d88860df0aac595ad3712833d13ea3
Patrick M. Jensen <patmjen@gmail.com>
2017-09-26 15:31:06 +0200

    Add script for preprocessing

---
+++ parse_wiki.py
@@ -0,0 +1,24 @@
+import sys
+import xml.etree.ElementTree as etree
+
+fname = sys.argv[1] if len(sys.argv) > 1 else 'wiki20k.xml'
+
+n = 0
+nfile = 0
+with open(fname) as xml:
+    chars_written = 0
+    crnt_f = open('{}-{}'.format(fname, nfile), 'w')
+    for event, elem in etree.iterparse(xml, events=('end',)):
+        if elem.tag.endswith('text'):
+            n += 1
+            chars_written += crnt_f.write(
+                elem.text.lower().replace('\n', ' ') + '\n'
+            )
+            if chars_written > 1e6:
+                crnt_f.close()
+                nfile += 1
+                chars_written = 0
+                crnt_f = open('{}-{}'.format(fname, nfile), 'w')
+
+
+print(n)
